76291406358f8b85de726a8d6e293dd8b0ae01ae
vfem <36859396+vfem@users.noreply.github.com>
2020-01-09 13:59:29 +0300

    Update ScheduleEntryRepository.java

--- src/main/java/org/vfem/schedule/ScheduleEntryRepository.java
+++ src/main/java/org/vfem/schedule/ScheduleEntryRepository.java
@@ -2,7 +2,7 @@ package org.vfem.schedule;
 
 import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called ScheduleEntryRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface ScheduleEntryRepository extends CrudRepository<ScheduleEntry, Integer> {
